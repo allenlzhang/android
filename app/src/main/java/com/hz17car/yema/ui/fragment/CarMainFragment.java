5fb88f8bb62e8dbd6e467ab0d3223d699ce5c167
liuxiangfei <francis.liu@carlt.com.cn>
2018-03-16 14:41:39 +0800

    no message

---
+++ app/src/main/java/com/hz17car/yema/ui/fragment/CarMainFragment.java
@@ -0,0 +1,18 @@
+package com.hz17car.yema.ui.fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.hz17car.yema.base.BaseFragment;
+
+/**
+ * Created by liu on 2018/3/16.
+ */
+
+public class CarMainFragment extends BaseFragment {
+    @Override
+    protected View inflateView(LayoutInflater inflater) {
+        return null;
+
+    }
+}
