bed526819ee9de555e00cdf6dfc7956c038b529d
liuxiangfei <francis.liu@carlt.com.cn>
2018-03-20 09:51:39 +0800

    远程

---
+++ app/src/main/java/com/carlt/yema/ui/activity/remote/RemoteLogActivity.java
@@ -0,0 +1,15 @@
+package com.carlt.yema.ui.activity.remote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.carlt.yema.R;
+
+public class RemoteLogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote_log);
+    }
+}
