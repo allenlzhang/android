8b28d3895353b57a500bb7fe223f40dd31860bf9
marller <marller.ma@carlt.com.cn>
2018-04-11 22:04:21 +0800

    +Add   1.   2.
      +Edit
      1.点击明文密文切换光标置于末尾
      2.

--- app/src/main/java/com/carlt/yema/SplashActivity.java
+++ app/src/main/java/com/carlt/yema/SplashActivity.java
@@ -23,9 +23,7 @@ import com.carlt.yema.utils.LocalConfig;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import okhttp3.Call;
 import okhttp3.Callback;
